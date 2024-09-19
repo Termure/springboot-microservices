113bc534fba88f05693f09ac6b067f9b43560458
Ioan Termure <ioan.termure24@gmail.com>
2024-09-19 14:39:59 +0300

    Defines DepartmentRepository class

---
+++ springboot-department-microservice/src/main/java/net/springboot/department_microservice/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package net.springboot.department_microservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import net.springboot.department_microservice.entity.Department;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long>{
+
+}
