b1639e62e78b8e4ea0da301f98d5a113a93503f1
Ioan Termure <ioan.termure24@gmail.com>
2024-10-08 14:24:17 +0300

    Implements microservice communication between employee and department service via WebFlux

--- springboot-employee-microservice/src/main/java/net/springboot/employee_microservice/service/impl/EmployeeServiceImpl.java
+++ springboot-employee-microservice/src/main/java/net/springboot/employee_microservice/service/impl/EmployeeServiceImpl.java
@@ -9,9 +9,8 @@ import net.springboot.employee_microservice.exception.ResourceNotFoundException;
 import net.springboot.employee_microservice.repository.EmployeeRepository;
 import net.springboot.employee_microservice.service.EmployeeService;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -19,7 +18,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
     private ModelMapper modelMapper;
-    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -31,10 +30,11 @@ public class EmployeeServiceImpl implements EmployeeService {
     public APIResponseDto getEmployeeById(Long id){
         Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee", "id", id));
 
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
-                DepartmentDto.class);
-
-        DepartmentDto departmentDto = responseEntity.getBody();
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
         APIResponseDto apiResponseDto = new APIResponseDto();
         apiResponseDto.setEmployee(modelMapper.map(employee, EmployeeDto.class));
