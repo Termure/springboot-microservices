a8de5b0e8f54c9f9e848e44b3e09076f67c206fd
Ioan Termure <ioan.termure24@gmail.com>
2024-11-07 16:37:32 +0200

    Test commit

--- springboot-department-microservice/src/main/java/net/springboot/department_microservice/SpringbootDepartmentMicroserviceApplication.java
+++ springboot-department-microservice/src/main/java/net/springboot/department_microservice/SpringbootDepartmentMicroserviceApplication.java
@@ -8,6 +8,7 @@ public class SpringbootDepartmentMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDepartmentMicroserviceApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
