61e11a9ab4a84f1aa2eeb07f236200496fd2e5f5
Ioan Termure <ioan.termure24@gmail.com>
2025-01-09 13:58:18 +0200

    Registers the department service as a Eureka client

--- springboot-department-microservice/src/main/java/net/springboot/department_microservice/SpringbootDepartmentMicroserviceApplication.java
+++ springboot-department-microservice/src/main/java/net/springboot/department_microservice/SpringbootDepartmentMicroserviceApplication.java
@@ -2,8 +2,10 @@ package net.springboot.department_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringbootDepartmentMicroserviceApplication {
 
 	public static void main(String[] args) {
