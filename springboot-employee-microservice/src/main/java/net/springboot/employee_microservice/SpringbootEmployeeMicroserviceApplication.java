9304fcefc238e0b66d13a89c38c7576644c1b309
Ioan Termure <ioan.termure24@gmail.com>
2025-01-09 14:07:25 +0200

    Registers the employee service as a Eureka client

--- springboot-employee-microservice/src/main/java/net/springboot/employee_microservice/SpringbootEmployeeMicroserviceApplication.java
+++ springboot-employee-microservice/src/main/java/net/springboot/employee_microservice/SpringbootEmployeeMicroserviceApplication.java
@@ -2,11 +2,13 @@ package net.springboot.employee_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringbootEmployeeMicroserviceApplication {
 
 	@Bean
