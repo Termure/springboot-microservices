02441cb0c15a02317ff3859c8c066ac7aaf0f9b2
Ioan Termure <ioan.termure24@gmail.com>
2025-01-09 11:35:49 +0200

    Adds spring registry service module

---
+++ service-registry/src/test/java/net/springboot/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springboot.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
